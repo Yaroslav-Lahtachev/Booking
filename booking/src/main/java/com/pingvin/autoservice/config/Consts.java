2b430ab1470b1ab003539caed83826502331ec1c
anzy1016 <anna.zyrina@netcracker.com>
2019-05-20 14:19:29 +0300

    Default Changelist

---
+++ booking/src/main/java/com/pingvin/autoservice/config/Consts.java
@@ -0,0 +1,6 @@
+package com.pingvin.autoservice.config;
+
+public class Consts {
+
+    final public static String MESSAGE_ABOUT_CHANGING_TIME = "Hello, unfortunately, your order on pingvin.com was delayed. We are happy to provide you new Dates. Good luck in your next try, dude.";
+}
